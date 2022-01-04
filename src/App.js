f46d835b0ebc470339940b54716392e1a9fa2b21
Stephen kihuni <stephenkihuni55@gmail.com>
2022-01-04 17:37:49 +0300

    modified chitchat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {AiFillWechat} from 'react-icons/ai';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-x-4">
+         <AiFillWechat className="w-12 h-12" />
+        <div className="chat-notification-content">
+          <h4 className="text-xl font-medium text-black">ChitChat</h4>
+          <p className="">You have a new message!</p>
+        </div>
+      </div>
     </div>
   );
 }
