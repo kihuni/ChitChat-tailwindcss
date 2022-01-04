8170982f21743d1c6d23cbe68522b1d879cd5adc
Stephen kihuni <stephenkihuni55@gmail.com>
2022-01-04 17:57:16 +0300

    style added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import "./App.css";
+
 import {AiFillWechat} from 'react-icons/ai';
 
 
 function App() {
   return (
     <div className="App">
-      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-x-4">
+      <div className="p-6 b max-w-sm mx-auto bg-white rounded-xl shadow-lg space-x-4">
          <AiFillWechat className="w-12 h-12" />
         <div className="chat-notification-content">
           <h4 className="text-xl font-medium text-black">ChitChat</h4>
